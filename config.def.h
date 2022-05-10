d666b47fc9041a9cb587ae88ffdc2520f64a1849
DragonMeido <srmajhi00@gmail.com>
2022-05-11 01:32:17 +0530

    trying xyz patch

--- config.def.h
+++ config.def.h
@@ -4,8 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"CodeNewRoman Nerd Font Mono:size=16"
 };
+
+static int dmx = 0; /* put dmenu at this x offset */
+static int dmy = 0; /* put dmenu at this y offset (measured from the bottom if topbar is 0) */
+static unsigned int dmw = 0; /* make dmenu this wide */
+
+
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
