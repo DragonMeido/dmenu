908086f6d2a7f37a7d397d5db2ee9a4e702f0c85
DragonMeido <srmajhi00@gmail.com>
2022-05-11 19:51:40 +0530

    still poking variables

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 2;
+static const unsigned int alpha = 0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"CodeNewRoman Nerd Font Mono:size=16"
